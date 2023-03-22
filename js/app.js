81b0fec9202a6f1f226ee5b014c82319b74589cb
Somiichan <cupichan_93@hotmail.com>
2023-03-22 14:26:55 +0100

    fin clase miercoles eventlistener click "hola" en check-out-button

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import './flip-element.js';
+import './social-networks.js';
+import './swipeable-element.js';
+import './modal.js';
---
+++ js/modal.js
@@ -0,0 +1,8 @@
+export default (() => {
+    
+    const checkButton = document.querySelector('.header-checkout-button');
+        checkButton.addEventListener("click", function(){
+            console.log("Hola");
+        })
+  
+})();

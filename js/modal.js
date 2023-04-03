9d134203fdd2fe0e5c56954057a0b66d3bd13447
Somiichan <cupichan_93@hotmail.com>
2023-04-03 13:53:21 +0200

    modal hecho final clase lunes 3.04

--- js/modal.js
+++ js/modal.js
@@ -8,4 +8,5 @@ export default (() => {
             modal.classList.toggle('active');
         });
     });
+    
 })();
--- js/swipeable-element.js
+++ js/swipeable-element.js
@@ -1,4 +1,11 @@
 export default (() => {
     
-  
+    const swipeButtons = document.querySelectorAll('.swipeable-button');
+    
+    swipeButtons.forEach(swipeButton => {
+        swipeButton.addEventListener('click', () => {
+            swipeButton.closest('.swipeable-element').classList.toggle('active');
+        });
+    });
+
 })();

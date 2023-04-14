299e73e1151b66a09c9b3cde46a40e943fa5c8af
Somiichan <cupichan_93@hotmail.com>
2023-04-14 14:26:32 +0200

    retoque en tabs

--- js/tabs.js
+++ js/tabs.js
@@ -3,28 +3,31 @@ export default (() => {
   const tabs = document.querySelectorAll('.tab');
   const contents = document.querySelectorAll('.content');
 
-    tabs.forEach((tab, index) => {
-      tab.addEventListener('click', () => {
-        selectTab(index);
-      });
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      selectTab(index);
+    });
+  });
+
+  function selectTab(index) {
+    tabs.forEach((tab, tabIndex) => {
+      if (tabIndex === index) {
+        tab.classList.add('active');
+      } else {
+        tab.classList.remove('active');
+      }
     });
 
-    function selectTab(index) {
-      tabs.forEach((tab, tabIndex) => {
-        if (tabIndex === index) {
-          tab.classList.add('active');
-        } else {
-          tab.classList.remove('active');
-        }
-      });
-      
-      contents.forEach((content, contentIndex) => {
-        if (contentIndex === index) {
-          content.classList.add('active');
-        } else {
-          content.classList.remove('active');
-        }
-      });
-    }
+    contents.forEach((content, contentIndex) => {
+      if (contentIndex === index) {
+        content.classList.add('active');
+      } else {
+        content.classList.remove('active');
+      }
+    });
     
+    $(".content").hide();
+    $(`.content:eq(${index})`).fadeIn(200);
+  }
+
 })();
